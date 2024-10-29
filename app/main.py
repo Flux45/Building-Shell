c6195ad5d96630b66791d4060dcaa09daa76039f
Flux <aayyuusshh.45@gmail.com>
2024-10-29 17:20:19 +0530

    type command

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
 
-    valid_commands = ['exit 0', 'echo']
+    valid_commands = ['exit 0', 'echo', 'exit', 'type']
 
     while True:
         sys.stdout.write("$ ")
@@ -12,7 +12,13 @@ def main():
         # Wait for user input
         command = input()
 
-        if command == 'exit 0':
+        if command.startswith('type') in valid_commands:
+            comm = command[5:]
+            if comm in valid_commands:
+                sys.stdout.write(f"{comm} is a shell builtin")
+            else:
+                sys.stdout.write(f"{comm}: not found\n")
+        elif command == 'exit 0':
             sys.exit(0)
         elif command.startswith('echo'):
             message = command[5:]
