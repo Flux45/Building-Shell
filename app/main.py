73c8b26350959a3e98c8256a7d515d55624e994c
Flux <aayyuusshh.45@gmail.com>
2024-10-30 14:51:53 +0530

    cd command

--- app/main.py
+++ app/main.py
@@ -7,25 +7,14 @@ from typing import Optional
 from os.path import expanduser
 from subprocess import call
 
-def locate_executable(command) -> Optional[str]:
-    path = os.environ.get("PATH", "")
-
-    for directory in path.split(":"):
-        file_path = os.path.join(directory, command)
-
-        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
-            return file_path
-
 
 def main():
-    commands = {"exit", "echo", "type", "pwd"}
+    commands = {"exit", "echo", "type"}
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
         # Wait for user input
-        comm = input()
-        command = comm.split()
-        user_command , args = comm.split(" ")
+        command = input().split()
         # if command[0] not in commands:
         #     print(f"${command[0]}: command not found")
         # elif command[0] == "exit" and command[1] == "0":
@@ -58,15 +47,85 @@ def main():
                 try:
                     os.chdir(" ".join(command[1:]))
                 except FileNotFoundError:
-                    print(command[0]+ ": " + command[1] + ": No such file or directory")
+                    print(" ".join(command) + ": No such file or directory")
             case _:
-                if executable := locate_executable(user_command):
-                    subprocess.run([executable, args])
+                if os.path.exists(command[0]):
+                    os.system(" ".join(command))
                 else:
                     # print("i'm here")
                     print(f"${command[0]}: command not found")
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+# def locate_executable(command) -> Optional[str]:
+#     path = os.environ.get("PATH", "")
+#
+#     for directory in path.split(":"):
+#         file_path = os.path.join(directory, command)
+#
+#         if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+#             return file_path
+#
+#
+# def main():
+#     commands = {"exit", "echo", "type", "pwd"}
+#     while True:
+#         sys.stdout.write("$ ")
+#         sys.stdout.flush()
+#         # Wait for user input
+#         comm = input()
+#         command = comm.split()
+#         user_command , args = comm.split(" ")
+#         # if command[0] not in commands:
+#         #     print(f"${command[0]}: command not found")
+#         # elif command[0] == "exit" and command[1] == "0":
+#         #     sys.exit(0)
+#         # elif command[0] == "echo":
+#         #     print(" ".join(command[1:]))
+#         match command[0]:
+#             case "exit":
+#                 if command[1] == "0":
+#                     sys.exit(0)
+#             case "echo":
+#                 print(" ".join(command[1:]))
+#             case "type":
+#                 if command[1] in commands:
+#                     print(f"{command[1]} is a shell builtin")
+#                 else:
+#                     paths = os.getenv("PATH").split(":")
+#                     # print(paths)
+#                     for path in paths:
+#                         path_to_command = f"{path}/{command[1]}"
+#                         # print(path_to_command)
+#                         if os.path.exists(path_to_command):
+#                             print(f"{command[1]} is {path_to_command}")
+#                             break
+#                     else:
+#                         print(f"{command[1]}: not found")
+#             case "pwd":
+#                 print(f"{os.getcwd()}")
+#             case "cd":
+#                 try:
+#                     os.chdir(" ".join(command[1:]))
+#                 except FileNotFoundError:
+#                     print(command[0]+ ": " + command[1] + ": No such file or directory")
+#             case _:
+#                 if executable := locate_executable(user_command):
+#                     subprocess.run([executable, args])
+#                 else:
+#                     # print("i'm here")
+#                     print(f"${command[0]}: command not found")
+# if __name__ == "__main__":
+#     main()
 
 
 
